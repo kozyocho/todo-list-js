3eb994e790081eab641773723b0c6e530a7ee1a3
yamasaki <shohei615615@gmail.com>
2024-10-15 18:21:58 +0900

    動的に要素を生成・追加する処理を追加

--- js/app.js
+++ js/app.js
@@ -12,9 +12,49 @@ function getInputValue() {
 
   //追加ボタンを押したら入力欄を空白にする
   input.value = "";
+
+  //生成した要素を未完了エリアに追加する関数を追加
+  addIncomplete(inputValue);
 }
 
 //追加ボタンを押したらgetInputValue要素を取得
 const addButton = document.getElementById("js-add");
 
 addButton.addEventListener("click", getInputValue);
+
+/*
+1. 入力内容を取得
+2. incomplete__list, incomplete__todo-content, incomplete__todo-content-textを生成
+3. ターゲットにするp要素を取得
+4. p要素のvalueに取得した内容を格納
+*/
+
+//要素を動的に生成する処理
+function createIncompleteElement() {
+  const incompleteListElement = document.createElement("div");
+  incompleteListElement.className = "incomplete__list";
+
+  //生成した要素を追加する親要素を取得
+  const parentElement = document.getElementById("parent");
+
+  //親要素の子要素として生成した要素を追加
+  parentElement.appendChild(incompleteListElement);
+
+  const incompleteTodoContent = document.createElement("div");
+  incompleteTodoContent.className = "incomplete__todo-content";
+  incompleteListElement.appendChild(incompleteTodoContent);
+
+  const incompleteTodoContentText = document.createElement("p");
+  incompleteTodoContentText.className = "incomplete__todo-content-text";
+  incompleteTodoContent.appendChild(incompleteTodoContentText);
+
+  return incompleteTodoContent;
+}
+
+function addIncomplete(inputvalue) {
+  //作成したp要素を取得
+  const target = createIncompleteElement();
+
+  //p要素に入力内容を格納
+  target.textContent = inputvalue;
+}
